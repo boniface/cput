eb26d627e4f5e80c8130823616db885aab5f2e9d
boniface <20Mataba04>
2014-02-11 09:51:21 +0200

    Added mult

---
+++ src/main/java/com/kabaso/cput/tp/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.kabaso.cput.tp;
+
+/**
+ *
+ * @author boniface
+ */
+public class Calculator {
+
+    int add(int a, int b) {
+        return a+b; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
